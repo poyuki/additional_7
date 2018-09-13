c9770a72c9767cb1edbc1eb58fd7424a978659f4
poyu <poyuki9@gmail.com>
2018-09-13 20:39:44 +0300

    let play sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+    let r=[],i,j,
+        sugestions='123456789';
+
+    for(i=0;i<9;i++){
+
+        for(j=0;j<9;j++){
+            r[i]=[];
+            if(!matrix[i][j]){
+                r[i].push(j);
+            }
+        }
+    }
+
+
+};
